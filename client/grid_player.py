ad36da1f21d8c11d2de2ade7af462fdffb673442
Kur <KyrelJerome@users.noreply.github.com>
2021-01-03 18:41:17 -0500

    Grid_player

--- client/grid_player.py
+++ client/grid_player.py
@@ -3,5 +3,6 @@ class GridPlayer:
     def __init__(self):
         self.foo = True
 
-    def tick(self, game_map, your_units, enemy_units, resources, turns_left):
+    def tick(self, game_map: Map, your_units: Units, enemy_units: Units,
+             resources: int, turns_left: int) -> [Move]:
         return []
